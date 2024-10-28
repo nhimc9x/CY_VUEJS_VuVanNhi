2cd04acce7cd0935ded75cd167f0396468f24349
nhimc9x <nhicomputer77@gmail.com>
2024-10-28 16:15:33 +0700

    BASE

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,11 @@ import { routes } from '@/router/index.js'
 </script>
 
 <template>
-  <div class="header">
-    <RouterLink v-for="item in routes" :key="item.name" :to="item.path">{{item.title}}</RouterLink>
-  </div>
-  <RouterView />
+<!--    <RouterLink v-for="item in routes" :key="item.name" :to="item.path">{{item.title}}</RouterLink>-->
+<!--  <RouterView />-->
+  <div>lorem ipsum</div>
 </template>
 
 <style scoped>
-.header {
-  position: fixed;
-  left: 0;
-  right: 0;
-  top: 0;
-  display: flex;
-  justify-content: center;
-  gap: 20px;
-  background-color: #2c3e50;
-}
+
 </style>
